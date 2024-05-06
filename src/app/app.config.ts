af68c717d4ac19efab3291458de64f7714410fd4
Luis Angel <luis.flores@durango.gob.mx>
2024-05-06 15:44:56 -0600

    Integrando datos a los detalles del producto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+// withComponentInputBinding id llega por medio de un input a la ruta
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient()
   ]
 };
--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
+import { ProductService } from '../../../shared/services/product.service';
+import { Product } from '@shared/models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+
+  @Input() id?: string;
+
+  product = signal<Product | null>(null);
+
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getOne(this.id).subscribe({
+        next: (product) => {
+          // console.log('product', product);
+          this.product.set(product);
+        },
+        error: (err) => {},
+      });
+    }
+
+  }
+
 
 }
