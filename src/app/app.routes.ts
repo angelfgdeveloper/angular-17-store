5026e8c3fb6b2d26c22c74134fb575091ba9e8e6
Luis Angel <luis.flores@durango.gob.mx>
2024-05-07 16:04:57 -0600

    uso de lazyloading en componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { ListComponent } from './domains/products/pages/list/list.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
+// import { ListComponent } from './domains/products/pages/list/list.component';
+// import { AboutComponent } from './domains/info/pages/about/about.component';
+// import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 
 import { LayoutComponent } from '@shared/components/layout/layout.component';
@@ -15,15 +15,17 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        component: ListComponent,
+        loadComponent: () => import('./domains/products/pages/list/list.component').then(m => m.ListComponent),
       },
       {
         path: 'about',
-        component: AboutComponent,
+        // component: AboutComponent,
+        loadComponent: () => import('./domains/info/pages/about/about.component'),
       },
       {
         path: 'product/:id',
-        component: ProductDetailComponent,
+        // component: ProductDetailComponent,
+        loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent),
       },
     ]
   },
